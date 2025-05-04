be1a1f5d2c5d857d2a36c529c11d7b205c4ba607
smutlak2021 <smutlak@tandratech.com>
2025-05-04 01:35:36 -0600

    initial 01

--- src/main/java/com/sameer/jpr03/JPR03.java
+++ src/main/java/com/sameer/jpr03/JPR03.java
@@ -4,13 +4,24 @@
 
 package com.sameer.jpr03;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  *
  * @author smutl
  */
 public class JPR03 {
 
+//    https://codefinity.com/courses/v2/190d2568-3d25-44d0-832f-da03468004c9/c0bcd017-ff39-46ec-bc93-acd569f3497d/d56d7f54-9992-491a-a50f-64ceee652f04
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> result = numbers.stream().map( b -> b*b).toList();
+        System.out.println(result);
+        
+        List<String> names = Arrays.asList("Alice", "Bob", "Charlie", "David", "Eva");
+        Integer res = names.stream().map( p -> p.length()).max((x, y) -> Integer.compare(x, y)).get();
+        System.out.println(res);
     }
 }
---
+++ src/main/java/com/sameer/sec02/Lec01LazyStream.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sameer.sec02;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author smutl
+ */
+public class Lec01LazyStream {
+
+    private static final Logger log = LoggerFactory.getLogger(Lec01LazyStream.class.getName());
+    
+    public static void main(String[] args){
+        
+    }
+}
